151fdb225ceffd9d4b77ab95b601510e97c6ad58
Ryan Sullenberger <sullenberger@gmail.com>
2018-05-22 23:23:53 -0400

    initial commit

---
+++ time_of_week_in_seconds.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from datetime import datetime, timedelta
+
+def current_time_to_sec(start, now):
+    seconds_of_week = (now - start).total_seconds()
+    return seconds_of_week
+
+now = datetime.now()
+timestart = (now - timedelta(days=now.weekday()+1)).replace(hour=0, minute=0, second=0, microsecond=0)
+delta = current_time_to_sec(timestart, now)
+
+print(delta)
